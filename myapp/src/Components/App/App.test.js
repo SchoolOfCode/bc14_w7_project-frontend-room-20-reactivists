7b97b789da8910f2d2859aa4f13399210f60d177
alicefirminger <alicefirminger@gmail.com>
2023-04-28 13:56:28 +0100

    began testing branch

--- myapp/src/Components/App/App.test.js
+++ myapp/src/Components/App/App.test.js
@@ -1,8 +1,18 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+import FilterDropDown from "../FilterDropdown/FilterDropDown";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test("renders learn react link", () => {
+	render(<App />);
+	const linkElement = screen.getByText(/learn react/i);
+	expect(linkElement).toBeInTheDocument();
+});
+
+//test that only css links display when selected from filter menu
+//
+
+test("Checks that when css is selected only css links are displayed", () => {
+	render(<FilterDropDown />);
+	const linkElement = screen.getByText(/learn react/i);
+	expect(linkElement).toBeInTheDocument();
 });
